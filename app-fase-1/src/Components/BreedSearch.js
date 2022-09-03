ff209be2040f3f479a4e42c8e98edf409d352655
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-09-02 20:42:26 -0600

    fix - Utilizar busqueda con login
    Ahora solo puede utilizar la busqueda cuando halla iniciado sesion

--- app-fase-1/src/Components/BreedSearch.js
+++ app-fase-1/src/Components/BreedSearch.js
@@ -1,83 +1,94 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import {useAuth0} from '@auth0/auth0-react'
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { useAuth0 } from "@auth0/auth0-react"
 
-import SearchFilter from './SearchFilter';
-import BreedList from './BreedList';
-import BreedInfo from './BreedInfo';
-import BreedDisplay from './BreedDisplay';
-import NavBar from './NavBar';
-
+import SearchFilter from "./SearchFilter"
+import BreedList from "./BreedList"
+import BreedInfo from "./BreedInfo"
+import BreedDisplay from "./BreedDisplay"
+import NavBar from "./NavBar"
 
 const BreedSearch = () => {
-  const {isAuthenticated} = useAuth0()
+	const { isAuthenticated } = useAuth0()
+  
+	//StateHook for Breeds data
+	const [breeds, setBreeds] = useState([]) //Empty because we want to retrieve the data from the api
+	const [searchFilter, setSearchFilter] = useState("") //Search bar for countries
+
+	useEffect(() => {
+		if (isAuthenticated){
+
+		console.log("Sending Get request to Dog-API")
+		axios
+			.get("https://api.thedogapi.com/v1/breeds?limit=200&page=0") //Get request to api
+			.then(response => {
+				console.log("Response Fulfilled")
+				const breedsResponse = response.data
+				console.log(breedsResponse)
+				setBreeds(breedsResponse) //countries are stored in the state
+			})
+		} else{
+			setBreeds([])
+			console.log("User must be logged in to send the request to Dog-API")
+		}
+	}, [])
+
+	//Function that shows breeds
+	const showBreedImages = () => {
+		if (searchFilter === "") {
+			return []
+		} else {
+			const regex = new RegExp(`${searchFilter}`, "gi") //Regular expression to get the name upper or lower case
+			const filteredNames = breeds.filter(breed => breed.name.match(regex) !== null) //Filter the names with that regex
+			if (filteredNames.length > 10) {
+				return <p>too many matches, specify another filter</p> //This must be done different, because Countries expect a ul element not a string, maybe we can change Countries to accept errors.
+			} else {
+				if (filteredNames.length === 1) {
+					console.log(filteredNames[0].id)
+					return <BreedInfo breedInfo={filteredNames[0]} />
+				} else {
+					const breedArrayFinal = filteredNames.map(breed => (
+						<BreedDisplay key={parseInt(breed.id)} breed={breed} onClickhandler={showButton} />
+					))
+					return <BreedList breedArray={breedArrayFinal} />
+				}
+			}
+		}
+	}
 
 
-  //StateHook for Breeds data
-  const [breeds, setBreeds] = useState([]); //Empty because we want to retrieve the data from the api
-  const [searchFilter, setSearchFilter] = useState(""); //Search bar for countries
+	//Handlers
+	//Handle when searchbox text it's modified
+	const handleSearchFilterChange = event => setSearchFilter(event.target.value)
 
+	const showButton = event => {
+		console.log(event.target.name)
+		setSearchFilter(event.target.name)
+	}
 
-  useEffect( () => {
-    console.log("Sending Get request to Dog-API");
-    axios
-      .get("https://api.thedogapi.com/v1/breeds?limit=200&page=0") //Get request to api
-      .then((response) => {
-        console.log("Response Fulfilled");
-        const breedsResponse = response.data;
-        console.log(breedsResponse);
-        setBreeds(breedsResponse); //countries are stored in the state
-      });
-  }, [])
+	if (isAuthenticated) {
+		return (
+			<div className="container">
+				<div className="row">
+					<NavBar />
+					<SearchFilter value={searchFilter} onChange={handleSearchFilterChange} />
+					{showBreedImages()}
+				</div>
+			</div>
+		)
+		
+	} else {
+		return (
+			<div className="container">
+				<div className="row">
+					<NavBar />
+					<h3>Para Buscar debe haber iniciado sesión.</h3>
+				</div>
+			</div>
+		)
+		
+	}
 
+}
 
-  //Function that shows breeds
-  const showBreedImages = () => {
-    if (searchFilter === "") {
-      return [];
-    } else {
-      const regex = new RegExp(`${searchFilter}`, "gi"); //Regular expression to get the name upper or lower case
-      const filteredNames = breeds.filter(
-        (breed) => breed.name.match(regex) !== null
-      ); //Filter the names with that regex
-      if (filteredNames.length > 10) {
-        return <p>too many matches, specify another filter</p>; //This must be done different, because Countries expect a ul element not a string, maybe we can change Countries to accept errors.
-      } else {
-        if (filteredNames.length === 1) {
-          console.log(filteredNames[0].id);
-          return <BreedInfo breedId={filteredNames[0]} />;
-        } else {
-          const breedArrayFinal = filteredNames.map((breed) => (
-            <BreedDisplay
-              key={parseInt(breed.id)}
-              breed={breed}
-              onClickhandler={showButton}
-            />
-          ));
-          return <BreedList breedArray={breedArrayFinal} />;
-        }
-      }
-    }
-  };
-
-  //Handlers
-  //Handle when searchbox text it's modified
-  const handleSearchFilterChange = (event) =>
-    setSearchFilter(event.target.value);
-
-  const showButton = (event) => {
-    console.log(event.target.name);
-    setSearchFilter(event.target.name);
-  };
-
-  return(
-    <div>
-      <NavBar />
-      {/* <h1>Dog Breeds!</h1> */}
-      <SearchFilter value={searchFilter} onChange={handleSearchFilterChange}/>
-      { showBreedImages() }
-    </div>
-  )
-};
-
-export default BreedSearch;
+export default BreedSearch
--- app-fase-1/src/Components/Profile.js
+++ app-fase-1/src/Components/Profile.js
@@ -1,26 +1,30 @@
-import React from "react";
-import {useAuth0} from '@auth0/auth0-react'
-import NavBar from "./NavBar";
+import React from "react"
+import { useAuth0 } from "@auth0/auth0-react"
+import NavBar from "./NavBar"
 
-const Profile = () =>{
-    const {user, isAuthenticated} = useAuth0()
-
-    if (isAuthenticated) {
-        return (            
-        <div>
-            <NavBar/>
-            <img scr = {user.picture}/>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-        </div>
-        )
-    }else{
-        return (
-            <div>
-             <NavBar/>
-             <h3>Para ver el Perfil debe Iniciar Sesión</h3>   
-            </div>
-        )
-    }
+const Profile = () => {
+	const { user, isAuthenticated } = useAuth0()
+	if (isAuthenticated) {
+		return (
+			<div className="container">
+				<div className="row">
+					<NavBar />
+					<img scr={user.picture} alt/>
+					<h2>{user.name}</h2>
+					<p>{user.email}</p>
+				</div>
+			</div>
+		)
+	} else {
+		return (
+			<div className="container">
+				<div className="row">
+					<NavBar />
+					<h3>Para ver el Perfil debe Iniciar Sesión</h3>
+				</div>
+			</div>
+		)
+	}
 }
-export default Profile;
+
+export default Profile
