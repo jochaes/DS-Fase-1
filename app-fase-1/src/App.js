641649edac6b01dd68782fe267addfbd69a92459
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-09-03 19:03:19 -0600

    Update App.js

--- app-fase-1/src/App.js
+++ app-fase-1/src/App.js
@@ -3,19 +3,24 @@ import { Outlet } from "react-router-dom";
 import './App.css';
 import Fondo from './Components/Fondo';
 import {useAuth0} from '@auth0/auth0-react'
-import Textos from './Components/Textos';
 import NavBar from './Components/NavBar';
 function App() {
   const {isAuthenticated} = useAuth0()
   return (
+
+
     <div className="App">
+              <NavBar />
       <div className='text-wrapper'>
+
         <h1>Dog Breed</h1>
         <p>Creado por: Brayan Gutierrez y Josue Chaves 
           Prototipo 1</p>
-         
+         <Fondo />
       </div>
     </div>
+
+
   );
 }
 
