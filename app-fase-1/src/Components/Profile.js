402b61976fa86f6b4bb92de47b045a715fa416ff
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-09-03 18:54:39 -0600

    Update Profile.js

--- app-fase-1/src/Components/Profile.js
+++ app-fase-1/src/Components/Profile.js
@@ -4,12 +4,14 @@ import NavBar from "./NavBar"
 
 const Profile = () => {
 	const { user, isAuthenticated } = useAuth0()
+
+	console.log(user);
 	if (isAuthenticated) {
 		return (
 			<div className="container">
 				<div className="row">
 					<NavBar />
-					<img scr={user.picture}/>
+					<img scr={user && user.picture} alt={user.nickname} width={'100'} />
 					<h2>{user.name}</h2>
 					<p>{user.email}</p>
 				</div>
